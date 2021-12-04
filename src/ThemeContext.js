11228fa78eb89d71659c10d48e4d1b06d88b5afc
Abosadiq <wafi.mo716@gmail.com>
2021-12-03 22:12:29 -0800

    New branch (#1)
    * first inital
    
    * added new styles for font
    
    * added box-sizing
    
    * set margin to 0
    
    * set padding to 0

---
+++ src/ThemeContext.js
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+export const ColorModeContext = React.createContext({
+	toggleColorMode: () => {},
+});
+
+export default function ToggleColorModeProvider({ children }) {
+	const [mode, setMode] = React.useState('light');
+	const colorMode = React.useMemo(
+		() => ({
+			toggleColorMode: () => {
+				setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+			},
+		}),
+		[]
+	);
+	const theme = React.useMemo(
+		() =>
+			createTheme({
+				palette: {
+					mode,
+				},
+			}),
+		[mode]
+	);
+	return (
+		<ColorModeContext.Provider value={colorMode}>
+			<ThemeProvider theme={theme}>{children}</ThemeProvider>
+		</ColorModeContext.Provider>
+	);
+}
